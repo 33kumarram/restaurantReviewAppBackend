916c5397ff0a35d4c709fc959124b29f13745991
33kumarram <95571520+33kumarram@users.noreply.github.com>
2023-05-06 22:23:04 +0530

    bug fix

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const ConnectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGOOSE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`MongoDB connected : ${connect.connection.host}`);
+    } catch (error) {
+        console.log(`Error : ${error.message}`);
+        process.exit()
+    }
+}
+
+module.exports = ConnectDB
